c8eff48b988fdabc4ebaace41647e6ef746c2c12
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-30 17:36:07 +0200

    file counter

--- count.sh
+++ count.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+count=$(find . -type f -o -type d -not -path '*/\.*' | wc -l)
 
-# Count the number of files and directories
-count=$(find . -type f -o -type d | grep -v '*/\.*' | wc -l)
 
-# Multiply the count by 5
 result=$((count * 5))
 
-
-# Use 'printf' to format and print the result
 printf "\tTotal files * 5: %d\t\n" "$result"
