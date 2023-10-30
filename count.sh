5f886ee2a0dc6815f8f1488adb7a8032c21076f9
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-30 17:27:03 +0200

    file counter

--- count.sh
+++ count.sh
@@ -6,8 +6,6 @@ count=$(find . -type f -o -type d | grep -v '/\.' | wc -l)
 # Multiply the count by 5
 result=$((count * 5))
 
-# Create the usage string
-usage="Usage: "
 
 # Use 'printf' to format and print the result
-printf "\t%s%d\t\n" "$usage" "$result"
+printf "\tTotal files * 5: %d\t\n" "$result"
