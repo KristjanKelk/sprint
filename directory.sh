31115d79d3f65efb065ad14cc28d555cbcec0dfc
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-30 13:56:42 +0200

    Found it

--- directory.sh
+++ directory.sh
@@ -1,3 +1,6 @@
-directory="theDirectory/left/down/beginning"
-file_to_print="README"
-cd "$directory" && cat "$file_to_print" || echo "Error: Directory or file not found."
+cd theDirectory
+cd left
+cd down
+cd beginning
+cat README
+pwd
