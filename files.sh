fff981da3b101824a81abad8332d1ff30e704e8e
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-30 17:53:01 +0200

    make, copy and remove

---
+++ files.sh
@@ -0,0 +1,17 @@
+# Create files a, !, \, and "
+touch a '!' '\\' '"'
+
+# Create a directory called `
+mkdir '`'
+
+# Copy the file ! into `
+cp '!' '`'
+
+# Check the MOVE_A environment variable
+if [ "$MOVE_A" = "yes" ]; then
+    # Move the file a into the ` directory
+    mv a '`'
+elif [ "$MOVE_A" = "no" ]; then
+    # Remove the file a
+    rm a
+fi
