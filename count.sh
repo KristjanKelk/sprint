a107a3cc6530485adc3ed8015fab1af67a85eca8
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-30 17:21:43 +0200

    file counter

--- count.sh
+++ count.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Use 'find' to count the number of files and directories
+# Count the number of files and directories
 count=$(find . -type f -o -type d | grep -v '/\.' | wc -l)
 
 # Multiply the count by 5
 result=$((count * 5))
 
-# Use 'printf' to format and print the result with tabs and newlines
-printf "\t\v%d\n" "$result"
+# Create the usage string
+usage="Usage: "
+
+# Use 'printf' to format and print the result
+printf "\v\t%s\t%d\v\n" "$usage" "$result"
