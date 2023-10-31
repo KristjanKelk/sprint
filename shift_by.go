981f86421cfad0816a4b8a23b512e87ef586dbf3
Kristjan Kelk <kristjankelk@gmail.com>
2023-10-31 13:58:36 +0200

    shifting runes

--- shift_by.go
+++ shift_by.go
@@ -1,11 +1,6 @@
 package sprint
 
 func ShiftBy(r rune, step int) rune {
-	if r < 'a' || r > 'z' {
-		//if r is not a-z, return r
-		return r
-	}
-
 	// if rune is a, set index to 0
 	index := int(r - 'a')
 
