51e5a3b75e1b6c3f084c505464d37cdfd31c361f
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-01 15:11:44 +0200

    added +1 to from and to in for loop

--- between_limits.go
+++ between_limits.go
@@ -3,13 +3,13 @@ package sprint
 func BetweenLimits(from rune, to rune) string {
 	if from < to {
 		runes := ""
-		for i := from; i < to; i++ {
+		for i := from + 1; i < to; i++ {
 			runes += string(i)
 		}
 		return runes
 	} else if from > to {
 		runes := ""
-		for i := to; i < from; i++ {
+		for i := to + 1; i < from; i++ {
 			runes += string(i)
 		}
 		return runes
