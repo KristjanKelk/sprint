d8b798559112369293c54d3e328fe0c3bf67e257
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-01 11:30:27 +0200

    added same output

--- int_vs_float.go
+++ int_vs_float.go
@@ -3,8 +3,9 @@ package sprint
 func IntVsFloat(i int, f float32) string {
 	if float32(i) > f {
 		return "integer"
+	} else if float32(i) == f {
+		return "same"
 	} else {
 		return "float"
 	}
-
 }
