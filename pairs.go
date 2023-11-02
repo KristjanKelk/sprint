31055ed838ea78b1cfbaf429054d8fbd588b1672
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-02 10:46:43 +0200

    pairs

---
+++ pairs.go
@@ -0,0 +1,17 @@
+package sprint
+
+import "fmt"
+
+func GenerateCombinations() string {
+	combinations := ""
+	for i := 10; i <= 99; i++ {
+		for j := i + 1; j <= 99; j++ {
+			combinations += fmt.Sprintf("%02d %02d, ", i, j)
+		}
+	}
+
+	if len(combinations) > 0 {
+		combinations = combinations[:len(combinations)-2]
+	}
+	return combinations
+}
