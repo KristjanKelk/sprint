dbe8520588ed3f66d2ca723e9932012aa231c8f1
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-02 17:24:27 +0200

    basic_atoi

---
+++ basic_atoi.go
@@ -0,0 +1,16 @@
+package sprint
+
+func BasicAtoi(s string) int {
+
+	newInteger := 0
+	//controlling if the digit is between 0 -9
+	for i := 0; i < len(s); i++ {
+		digit := int(s[i] - '0')
+		if digit < 0 || digit > 9 {
+			return 0 // Invalid character in the string
+		}
+		newInteger = newInteger*10 + digit
+	}
+
+	return newInteger
+}
