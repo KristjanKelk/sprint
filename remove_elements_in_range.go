eb5f500c55a279f6392d7a7ed37038a1979cfcb9
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-06 15:55:23 +0200

    bugs

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -1,30 +1,27 @@
 package sprint
 
 func RemoveElementsInRange(arr []float64, from, to int) []float64 {
-	excludeSlice := []float64{}
-
+	// Ensure from and to are within valid bounds
 	if from < 0 {
 		from = 0
 	}
-	if from >= len(arr) {
+	if from > len(arr) {
 		from = len(arr)
 	}
 	if to < 0 {
 		to = 0
 	}
-	if to >= len(arr) {
+	if to > len(arr) {
 		to = len(arr)
 	}
 
+	// Ensure from is less than or equal to to
 	if from > to {
 		from, to = to, from
 	}
 
-	for i := 0; i < len(arr); i++ {
-		if i < from || i >= to {
-			excludeSlice = append(excludeSlice, arr[i])
-		}
-	}
+	// Create a new slice with elements outside the specified range
+	modifiedArray := append(arr[:from], arr[to:]...)
 
-	return excludeSlice
+	return modifiedArray
 }
