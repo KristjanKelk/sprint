1cb777fc2d6053c39df0f3a13db79f91dee25006
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-06 18:33:23 +0200

    sort_integer_tabel

---
+++ sort_integer_table.go
@@ -0,0 +1,28 @@
+func SortIntegerTable(table []int) []int {
+	max:=tabel[0]
+	result:= []int
+	for i:=0, i < len(arr); i++{
+		if max < tabel[i]{
+			max = tabel[i]
+		}
+	}
+
+	for len(tabel) > 0 {
+		min:= 0
+		for i:=0; i < len(tabel); i++{
+			if tabel[i] < tabel[min] {
+				min = i
+			}
+
+		}
+		result = append(result, tabel[min])
+		tabel = append(tabel[:min], tabel[min+1]...)
+		
+	}
+
+	}
+	
+
+		
+
+
