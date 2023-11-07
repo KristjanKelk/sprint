6c662047999c56b73d905fd677a37c1db70ebf13
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-07 13:42:36 +0200

    isNumeric fix

--- is_numeric.go
+++ is_numeric.go
@@ -3,8 +3,8 @@ package sprint
 func IsNumeric(s string) bool {
 	for i := 0; i < len(s); i++ {
 		if rune(s[i]) < '0' || rune(s[i]) > '9' {
+			return false
 		}
-		return false
 	}
 	return true
 }
