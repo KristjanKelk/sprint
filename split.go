0d417fa4bbc5168545045631052c96bc9eab4e30
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-08 12:45:31 +0200

    as

--- split.go
+++ split.go
@@ -3,30 +3,28 @@ package sprint
 func Split(s, sep string) []string {
 	words := []string{}
 	word := ""
-	separatorPos := 0
+	separatorMatched := 0
+
 	for i := 0; i < len(s); i++ {
-		if s[i] == sep[separatorPos] {
-			separatorPos++
-			if separatorPos == len(sep) {
+		if s[i] == sep[separatorMatched] {
+			separatorMatched++
+			if separatorMatched == len(sep) {
 				// Found the full separator sequence
-				if word != "" {
-					words = append(words, word)
-					word = ""
-				}
-				separatorPos = 0
+				separatorMatched = 0
+				words = append(words, word)
+				word = ""
 			}
 		} else {
-			if separatorPos > 0 {
-				// Append any partially matched separator runes to the word
-				word += sep[:separatorPos]
-				separatorPos = 0
-			}
+			// Append any partially matched separator runes to the word
+			word += sep[:separatorMatched]
+			separatorMatched = 0
 			word += string(s[i])
 		}
 	}
-	if word != "" {
-		words = append(words, word)
-	}
+
+	// Append any remaining characters to the last word
+	word += sep[:separatorMatched]
+	words = append(words, word)
 
 	return words
 }
