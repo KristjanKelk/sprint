fefa8fd7b0eaa11f5c7ae59840b74054284ccaa2
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-08 16:47:34 +0200

    fix

--- nbr_base.go
+++ nbr_base.go
@@ -16,20 +16,24 @@ func NbrBase(n int, base string) string {
 	// Step 2: Convert the integer to the specified base
 	var result string
 	if n < 0 {
-		result = "-"
 		n = -n
-	}
-	for n > 0 {
-		remainder := n % len(base)
-		result = string(base[remainder]) + result
-		n = n / len(base)
+		for n > 0 {
+			remainder := n % len(base)
+			result = string(base[remainder]) + result
+			n = n / len(base)
+		}
+		result = "-" + result
+	} else {
+		for n > 0 {
+			remainder := n % len(base)
+			result = string(base[remainder]) + result
+			n = n / len(base)
+		}
 	}
 
 	// Step 3: Handle negative numbers and return the result
 	if result == "" {
 		result = "0"
-	} else if result == "-" {
-		result = string(base[0]) + result
 	}
 	return result
 }
