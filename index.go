c75ed296d411643fb2c2f0b8970c36cc09dba6e8
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-08 15:22:54 +0200

    compare

--- index.go
+++ index.go
@@ -2,8 +2,11 @@ package sprint
 
 func Index(s string, toFind string) int {
 	for i := 0; i <= len(s)-len(toFind); i++ {
+
 		match := true
+
 		for j := 0; j < len(toFind); j++ {
+
 			if toFind[j] != s[i+j] {
 				match = false
 				break
