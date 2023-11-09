3c979965adaec07ed7d89d1aa7e12af3e3a4ad98
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-09 13:57:19 +0200

    fix

--- point_diff.go
+++ point_diff.go
@@ -6,6 +6,10 @@ type Point struct {
 	Text string
 }
 
+func MakePoint(x, y float32, text string) Point {
+	return Point{X: x, Y: y, Text: text}
+}
+
 func PointDiff(p1, p2 Point, text string) Point {
 	if p1.X > p2.X || (p1.X == p2.X && p1.Y > p2.Y) {
 		return p1
