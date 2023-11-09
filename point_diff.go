8a07541bd1ef528b6fb15359cf8ff22af3040159
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-09 13:41:10 +0200

    fix

--- point_diff.go
+++ point_diff.go
@@ -1,15 +1,15 @@
 package sprint
 
 type Point struct {
-	P1 float32
-	P2 float32
+	X int
+	Y int
 }
 
 func PointDiff(p1, p2 Point) Point {
-	if p1.P1+p1.P2 < p2.P1+p2.P2 {
-		return Point{P1: p1.P1, P2: p1.P2}
-	} else if p1.P1+p1.P2 > p2.P1+p2.P2 {
-		return Point{P1: p2.P1, P2: p2.P2}
+	if p1.X+p1.Y < p2.X+p2.Y {
+		return p1
+	} else if p1.X+p1.Y > p2.X+p2.Y {
+		return p2
 	}
-	return Point{P1: p1.P1, P2: p1.P2}
+	return p1
 }
