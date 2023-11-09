ff0e829d2609b4f4ff6522149c7b56735d3baaf1
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-09 13:15:25 +0200

    asd

--- make_point.go
+++ make_point.go
@@ -1,13 +1,13 @@
 package sprint
 
 // Point represents text at a specific point with X and Y coordinates.
-type Point struct {
+type Point1 struct {
 	X    float32
 	Y    float32
 	Text string
 }
 
 // MakePoint creates and returns a new Point struct with the specified values.
-func MakePoint(x, y float32, text string) Point {
-	return Point{X: x, Y: y, Text: text}
+func MakePoint(x, y float32, text string) Point1 {
+	return Point1{X: x, Y: y, Text: text}
 }
--- point_diff.go
+++ point_diff.go
@@ -1,15 +1,15 @@
 package sprint
 
 type Point struct {
-	X int
-	Y int
+	X float32
+	Y float32
 }
 
 func PointDiff(p1, p2 Point) Point {
 
-	if int(p1.X+p1.Y) < int(p2.X+p2.Y) {
+	if p1.X+p1.Y < p2.X+p2.Y {
 		return p2
-	} else if int(p1.X+p1.Y) > int(p2.X+p2.Y) {
+	} else if p1.X+p1.Y > p2.X+p2.Y {
 		return p1
 	}
 	return p2
