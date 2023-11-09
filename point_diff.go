357d460144e9f681febf8fc6686d964cf6e67907
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-09 13:59:08 +0200

    fix

--- point_diff.go
+++ point_diff.go
@@ -10,7 +10,7 @@ func MakePoint(x, y float32, text string) Point {
 	return Point{X: x, Y: y, Text: text}
 }
 
-func PointDiff(p1, p2 Point, text string) Point {
+func PointDiff(p1 Point, p2 Point, text string) Point {
 	if p1.X > p2.X || (p1.X == p2.X && p1.Y > p2.Y) {
 		return p1
 	}
