c99dc20b73cfabde1fd546a049909e073bb194f2
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-09 13:05:47 +0200

    fix

--- point_diff.go
+++ point_diff.go
@@ -6,8 +6,11 @@ type Point struct {
 }
 
 func PointDiff(p1, p2 Point) Point {
-	if p1.X > p2.X || (p1.X == p2.X && p1.Y > p2.Y) {
+
+	if p1.X+p1.Y < p2.X+p2.Y {
+		return p2
+	} else if p1.X+p1.Y > p2.X+p2.Y {
 		return p1
 	}
-	return p2
+	return p2, p1
 }
