ce6acb168a50542760aba89b7828d1dd207a833b
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-13 16:08:04 +0200

    find next prime

---
+++ find_next_prime.go
@@ -0,0 +1,25 @@
+package sprint
+
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func FindNextPrime(n int) int {
+	if n%2 == 0 {
+		n++
+	}
+
+	for !isPrime(n) {
+		n += 2
+	}
+
+	return n
+}
