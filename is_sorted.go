3b667cc388c7083a00cda7442beb1a2ae952a844
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-14 15:47:26 +0200

    fix

--- is_sorted.go
+++ is_sorted.go
@@ -18,7 +18,7 @@ func IsSorted(f func(a, b string) int, arr []string) bool {
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == result[i] {
 			continue
-		} else if arr[i] == result[len(result)-i] {
+		} else if arr[i] == result[(len(result)-1)-i] {
 			return true
 		} else {
 			return false
