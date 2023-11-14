2f5ec018ab319893c4490035b76a086692305048
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-14 15:12:02 +0200

    is sorted

---
+++ is_sorted.go
@@ -0,0 +1,40 @@
+package sprint
+
+func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
+	result := make([]string, len(a))
+	copy(result, a) // Copy the original slice to avoid modifying it
+
+	// Sorting the slice using the provided comparison function
+	for i := 0; i < len(result)-1; i++ {
+		for j := i + 1; j < len(result); j++ {
+			if f(result[i], result[j]) > 0 {
+				// Swap elements if they are in the wrong order
+				result[i], result[j] = result[j], result[i]
+			}
+		}
+	}
+
+	if copy(result, a) == result {
+		return true {
+			else {
+				false
+			}
+		}
+	}
+}
+
+func Compare(a, b string) int {
+	minLength := len(a)
+	if len(b) < minLength {
+		minLength = len(b)
+	}
+
+	for i := 0; i < minLength; i++ {
+		diff := int(a[i]) - int(b[i])
+		if diff != 0 {
+			return diff
+		}
+	}
+
+	return len(a) - len(b)
+}
