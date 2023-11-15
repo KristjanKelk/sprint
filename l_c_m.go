a6e0a696795c0de99f3b1ad0ecc164d2dff076f9
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-15 11:30:02 +0200

    lcm

---
+++ l_c_m.go
@@ -0,0 +1,20 @@
+package sprint
+
+func LCM(a, b int) int {
+	larger := a
+	smaller := b
+	if larger < b {
+		larger = b
+		smaller = a
+	}
+
+	if smaller == 0 {
+		return larger
+	}
+
+	for i := larger; ; i += larger {
+		if i%a == 0 && i%b == 0 {
+			return i
+		}
+	}
+}
