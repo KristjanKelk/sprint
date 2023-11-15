48ed562578d7410ad463f8fdc1c3f49be39c2732
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-15 10:36:09 +0200

    fix

--- g_c_d.go
+++ g_c_d.go
@@ -1,12 +1,17 @@
 package sprint
 
 func GCD(a, b int) int {
-	min := a
-	if min < b {
-	} else {
-		min = b
-	}
 	result := 1
+	min := a
+	if min > b {
+		min = b
+	} else if a == b {
+		result = a
+		return result
+	} else if a == 0 || b == 0 {
+		return 0
+	}
+
 	for i := min / 2; i > 0; i-- {
 		if a%i == 0 && b%i == 0 {
 			result = i
