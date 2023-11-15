09022b784f2f48df53865fecce46c791f68bedfd
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-15 12:07:41 +0200

    are thet anagrams

---
+++ are_anagrams.go
@@ -0,0 +1,27 @@
+package sprint
+
+import (
+	"sort"
+	"strings"
+)
+
+func AreAnagrams(str1, str2 string) bool {
+	// Remove leading and trailing whitespaces, and convert to lowercase
+	a := strings.ToLower(strings.TrimSpace(str1))
+	b := strings.ToLower(strings.TrimSpace(str2))
+
+	// Convert strings to slices of characters
+	aChars := strings.Split(a, "")
+	bChars := strings.Split(b, "")
+
+	// Sort the slices
+	sort.Strings(aChars)
+	sort.Strings(bChars)
+
+	// Join the sorted slices back into strings
+	sortedA := strings.Join(aChars, "")
+	sortedB := strings.Join(bChars, "")
+
+	// Compare the sorted strings
+	return sortedA == sortedB
+}
