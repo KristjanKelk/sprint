0a62732628aec3f6de35f1565d66e8f2da1cd64d
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-16 11:44:17 +0200

    fix

--- payout.go
+++ payout.go
@@ -18,8 +18,8 @@ func Payout(amount int, denominations []int) (payout []int) {
 
 func bubbleSort(arr []int) {
 	n := len(arr)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n-i; j++ {
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
 			if arr[j] < arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
