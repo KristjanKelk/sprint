da07c0d3d38745f7b411ade159b56faa13def091
Kristjan Kelk <kristjankelk@gmail.com>
2023-11-16 14:02:22 +0200

    overlapped

--- overlap.go
+++ overlap.go
@@ -1,5 +1,35 @@
 package sprint
 
 func Overlap(arr1, arr2 []int) []int {
-	return []int{}
+	if len(arr1) < len(arr2) {
+		arr1, arr2 = arr2, arr1
+	}
+	overlapped := make(map[int]bool)
+	for _, val := range arr1 {
+		overlapped[val] = false
+	}
+	result := []int{}
+	bubbleSort(arr2)
+
+	for i := 0; i < len(arr2); i++ {
+		for j := 0; j < len(arr1); j++ {
+			if arr1[j] == arr2[i] && !overlapped[arr2[i]] {
+				result = append(result, arr2[i])
+				overlapped[arr2[i]] = true
+				break
+			}
+		}
+	}
+	return result
+}
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] < arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
 }
--- payout.go
+++ payout.go
@@ -25,17 +25,18 @@ func Payout(amount int, denominations []int) (payout []int) {
 	return []int{}
 }
 
-func bubbleSort(arr []int) {
-	n := len(arr)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if arr[j] < arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
+/*
+	func bubbleSort(arr []int) {
+		n := len(arr)
+		for i := 0; i < n-1; i++ {
+			for j := 0; j < n-i-1; j++ {
+				if arr[j] < arr[j+1] {
+					arr[j], arr[j+1] = arr[j+1], arr[j]
+				}
 			}
 		}
 	}
-}
-
+*/
 func summ(arr []int) int {
 	sum := 0
 	for _, val := range arr {
